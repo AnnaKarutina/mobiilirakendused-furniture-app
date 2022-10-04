425d08156b42e0a8b9fd1c38ef68f46a2fd9a051
Anna at Mac class <anna.karutina@voco.ee>
2022-10-04 14:21:42 +0300

    Create Sign in screen - login with google is not complete

--- App.js
+++ App.js
@@ -7,11 +7,12 @@ import {
 } from 'react-native';
 
 import Splash from "./src/screens/auth/Splash";
+import Signin from "./src/screens/auth/Signin"
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Splash/>
+      <Signin/>
     </SafeAreaView>
   );
 };
