25cee97cc2f178ce50ec76b87ceb67027424247d
Anna at Mac class <anna.karutina@voco.ee>
2022-10-26 13:59:04 +0300

    Build Categories List - begining

--- src/screens/app/Home/index.js
+++ src/screens/app/Home/index.js
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { ScrollView, Text } from 'react-native';
+import { FlatList, View } from 'react-native';
 import { styles } from './styles';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import Header from "../../../components/Header";
 
+import { categories } from '../../../data/categories';
+
 const Home = () => {
+  const renderCategoryItem = ({ item, index }) => {
+    console.log(item)
+    return (
+      <Text>{item.title}</Text>
+    )
+  }
+
   return (
     <SafeAreaView>
       <Header showSearch title="Find All You Need" />
+      <FlatList
+        data={categories}
+        renderItem={renderCategoryItem}
+        keyExtractor={item => item.id}
+      />
     </SafeAreaView>
   )
 }
