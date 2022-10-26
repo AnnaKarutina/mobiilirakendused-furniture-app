c387e5dce387404aa22a6fea23e153e6a8f8bcde
Anna at Mac class <anna.karutina@voco.ee>
2022-10-26 13:49:01 +0300

    Build App Header

--- src/screens/app/Home/index.js
+++ src/screens/app/Home/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ScrollView, Text } from 'react-native';
 import { styles } from './styles';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import Header from "../../../components/Header";
+
 const Home = () => {
   return (
     <SafeAreaView>
-      <ScrollView style={styles.container}>
-        <Text>HOME</Text>
-      </ScrollView>
+      <Header showSearch title="Find All You Need" />
     </SafeAreaView>
   )
 }
