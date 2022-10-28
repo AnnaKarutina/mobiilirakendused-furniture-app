976acfa4a04a2ac08f20757be2591a1573bfe35a
Anna at Mac class <anna.karutina@voco.ee>
2022-10-28 12:08:15 +0300

    Build Basic Profile Screen

--- src/screens/app/Profile/index.js
+++ src/screens/app/Profile/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { ScrollView, Text } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 import { styles } from './styles';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Header from '../../../components/Header';
+import Button from "../../../components/Button";
+import ListItem from "../../../components/ListItem";
 
 const Profile = () => {
+  const num = 10;
+
+  const onLogout = () => {
+    console.log('log out clicked');
+  }
+
   return (
-    <SafeAreaView>
-      <ScrollView style={styles.container}>
-        <Text>Profile</Text>
-      </ScrollView>
+    <SafeAreaView style={{ flex:1 }}>
+      <Header title="Profile" showLogout onLogout={onLogout} />
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <Text style={styles.name}>User name</Text>
+          <Text style={styles.email}>User email</Text>
+
+          <ListItem title="My Listings" subtitle={`You have ${num} listings`} />
+          <ListItem title="Settings" subtitle="Account, FAQ, Contact" />
+        </View>
+
+        <Button style={{ flex: 0 }} title="Add New Listing" />
+      </View>
     </SafeAreaView>
   )
 }
