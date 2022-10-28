20cfc79e4320ce49942a076bfbf52dda3d625e13
Anna at Mac class <anna.karutina@voco.ee>
2022-10-28 10:36:18 +0300

    Build Favorites List

--- src/screens/app/Favorites/index.js
+++ src/screens/app/Favorites/index.js
@@ -3,11 +3,23 @@ import { ScrollView, Text } from 'react-native';
 import { styles } from './styles';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import Header from "../../../components/Header";
+import { products } from "../../../data/products";
+import { FlatList } from "react-native";
+import FavoriteItem from "../../../components/FavoriteItem";
+
 const Favorites = () => {
+  const renderItem = ({item}) => {
+    return (
+      <FavoriteItem {...item} />
+    )
+  }
+
   return (
     <SafeAreaView>
       <ScrollView style={styles.container}>
-        <Text>Favorites</Text>
+        <Header title="Favorites" />
+        <FlatList data={products} renderItem={renderItem} keyExtractor={(item) => String(item?.id)} />
       </ScrollView>
     </SafeAreaView>
   )
